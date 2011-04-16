b0f50f0e019fbbb18639dbe23bd19347adc2f991
alex.zenon <zenon.alex@gmail.com>
2011-04-16 16:35:26 -0700

    ref+comments+arguments

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -10,6 +10,11 @@ authorization do
     has_permission_on [:comments,:arguments], :to => [:edit, :update,:destroy] do
       if_attribute :user => is { user }
     end
+    has_permission_on :users, :to => [:index, :show, :new, :create]
+    has_permission_on :users, :to => [:edit, :update, :destroy] do
+      if_attribute :user => is { user }
+    end
+    
   end
   role :guest do
     has_permission_on [:comments,:arguments], :to =>  [:index, :show]
@@ -17,5 +22,6 @@ authorization do
     has_permission_on [:comments,:arguments], :to => [:edit, :update,:destroy] do
       if_attribute :user => is { user }
     end
+    has_permission_on :users, :to => [:index, :show, :new, :create]
   end
 end
