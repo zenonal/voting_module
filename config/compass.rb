88f14335f447069d73181a6813055c3a57580a8e
alex.zenon <zenon.alex@gmail.com>
2011-06-20 17:31:19 +0200

    compas fix?

--- config/compass.rb
+++ config/compass.rb
@@ -6,8 +6,8 @@ environment  = Compass::AppIntegration::Rails.env
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-# css_dir   = 'tmp/stylesheets'
-# sass_dir  = 'app/stylesheets'
+#css_dir   = '/stylesheets'
+#sass_dir  = '/stylesheets'
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
