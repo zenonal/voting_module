858b5f248a25170608310e6c601f19af32259ca3
alex.zenon <zenon.alex@gmail.com>
2011-06-21 21:07:24 +0200

    monor

--- config/environment.rb
+++ config/environment.rb
@@ -14,3 +14,6 @@ BILL_AMENDMENTS_DURATION = 1.month
 BILL_VALIDATION_DURATION = 1.month
 
 INITIATIVE_LEVELS = ["", I18n.t("initiatives.level1"), I18n.t("initiatives.level2"), I18n.t("initiatives.level3"), I18n.t("initiatives.level4")]
+INITIATIVE_PHASES = ["", I18n.t("initiatives.phase0"), I18n.t("initiatives.phase1"),I18n.t("initiatives.phase2"),I18n.t("initiatives.phase3"),I18n.t("initiatives.phase4"),I18n.t("initiatives.phase5")]
+REFERENDUM_LEVELS = ["", I18n.t("referendums.level1"), I18n.t("referendums.level2"), I18n.t("referendums.level3"), I18n.t("referendums.level4")]
+REFERENDUM_PHASES = ["", I18n.t("referendums.phase3"),I18n.t("referendums.phase4"),I18n.t("referendums.phase5")]
