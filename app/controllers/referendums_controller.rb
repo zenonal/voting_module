5df445645f84b6b15725f3085ab8afb0403cebdf
alex.zenon <zenon.alex@gmail.com>
2011-06-23 06:44:45 +0200

    new changes

--- app/controllers/referendums_controller.rb
+++ app/controllers/referendums_controller.rb
@@ -2,7 +2,7 @@ class ReferendumsController < ApplicationController
   filter_resource_access
   before_filter :authenticate_user!, :except => [:show,:index]
   
-  ssl_required :show
+  ssl_exceptions :show
   
   # GET /referendums
   # GET /referendums.xml
