644497643e927e469bcfee70b5173cb51f3c5ee1
alex.zenon <zenon.alex@gmail.com>
2011-07-10 09:40:00 +0200

    new fix for tanker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -110,7 +110,7 @@ class ApplicationController < ActionController::Base
                                 @bills = eval( "@bills.find_all {|b| b.content_#{I18n.locale} != ''}" )
                                 a = Amendment.search_tank(params[:search], :paginate => false, :function => 1)
                                 a = a.find_all {|b| b.class.name == "Amendment" }
-                                @bills = @bills + eval(" a.find_all {|b| b.amendmentable_type == #{params[:controller]} } ")
+                                @bills = @bills + eval(" a.find_all {|b| b.amendmentable_type == bills.first.class.name } ")
                         end
                 else
                         @bills = []
