74ed51c326c9434636f95994c3ec05bd368f0b21
alex.zenon <zenon.alex@gmail.com>
2011-11-23 21:40:37 +0100

    fr translation

--- app/controllers/language_controller.rb
+++ app/controllers/language_controller.rb
@@ -2,19 +2,19 @@ class LanguageController < ApplicationController
   def fra
     I18n.locale = :fr
     Rails.cache.write("locales_page", true)
-    redirect_to session[:jumpback] 
+    redirect_to root_url
 	end
 	
   def eng
     I18n.locale = :en
     Rails.cache.write("locales_page", true)
-    redirect_to session[:jumpback] 
+    redirect_to root_url
 	end
 	
   def ndl
     I18n.locale = :nl
     Rails.cache.write("locales_page", true)
-    redirect_to session[:jumpback] 
+    redirect_to root_url
 	end
 
   def info
