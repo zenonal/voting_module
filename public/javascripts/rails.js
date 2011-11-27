55bacead246ed3d810926348d8974a7efd03eaf8
alex.zenon <zenon.alex@gmail.com>
2011-11-27 21:56:50 +0100

    rails.js fied

--- public/javascripts/rails.js
+++ public/javascripts/rails.js
@@ -1,5 +1,3 @@
-
-
 (function() {
   // Technique from Juriy Zaytsev
   // http://thinkweb2.com/projects/prototype/detecting-event-support-without-browser-sniffing/
@@ -173,5 +171,21 @@
       input.removeAttribute('data-original-value');
       input.disabled = false;
     });
+  });
+
+  Ajax.Responders.register({
+    onCreate: function(request) {
+      var csrf_meta_tag = $$('meta[name=csrf-token]')[0];
+
+      if (csrf_meta_tag) {
+        var header = 'X-CSRF-Token',
+            token = csrf_meta_tag.readAttribute('content');
+
+        if (!request.options.requestHeaders) {
+          request.options.requestHeaders = {};
+        }
+        request.options.requestHeaders[header] = token;
+      }
+    }
   });
 })();
