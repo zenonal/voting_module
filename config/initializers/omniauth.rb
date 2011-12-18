cb0e6a68b2a82f0f2dea5cf9e4f9ceaba29c897c
alex.zenon <zenon.alex@gmail.com>
2011-12-18 11:30:23 +0100

    omniauth problem solved: add the setup option

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
                 
         else
                 provider :facebook, '112773305478994', 'dca7a2c9691e5f513909f89efbd190a5',
-                        {:scope => 'email', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}, :setup => true
+                        {:setup => true, :scope => 'email', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
         end
   provider :google_apps, OpenID::Store::Filesystem.new('./tmp'), :domain => 'gmail.com'
   provider :open_id, OpenID::Store::Filesystem.new('/tmp')
