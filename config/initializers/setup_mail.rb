dfba9151b2036847cec2ad2e0f4d98ddbc93423c
alex.zenon <zenon.alex@gmail.com>
2011-12-26 17:58:57 +0100

    fiw pwd

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "localhost",
-  :user_name            => "zenon.alex@gmail.com",
-  :password             => "rdls112a",
+  :user_name            => "info@jegouverne.be",
+  :password             => "28rM1000Bxl",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
@@ -13,8 +13,8 @@ else
           :address              => "smtp.gmail.com",
           :port                 => 587,
           :domain               => "votingmodule.heroku.com",
-          :user_name            => "zenon.alex@gmail.com",
-          :password             => "rdls112a",
+          :user_name            => "info@jegouverne.be",
+          :password             => "28rM1000Bxl",
           :authentication       => "plain",
           :enable_starttls_auto => true
         }
