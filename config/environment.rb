4e3a15d8c027cadc211778d8be3f6e11df324b0a
alex.zenon <zenon.alex@gmail.com>
2012-02-12 13:14:36 +0100

    longer validation

--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,7 @@ EXCLUSION_THRESHOLD = 3
 BILL_VOTING_DURATION = 15.days
 BILL_EDITING_DURATION = 5.days
 BILL_AMENDMENTS_DURATION = 15.days
-BILL_VALIDATION_DURATION = 15.days
+BILL_VALIDATION_DURATION = 30.days
 
 INITIATIVE_LEVELS = ["", I18n.t("initiatives.level1"), I18n.t("initiatives.level2"), I18n.t("initiatives.level3"), I18n.t("initiatives.level4")]
 INITIATIVE_PHASES = ["", I18n.t("initiatives.phase0"), I18n.t("initiatives.phase1"),I18n.t("initiatives.phase2"),I18n.t("initiatives.phase3"),I18n.t("initiatives.phase4"),I18n.t("initiatives.phase5")]
